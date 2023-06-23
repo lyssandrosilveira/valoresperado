e28ac1e7b95ae67263230664585e5749c25cbec9
lyssandrosilveira <45230721+lyssandrosilveira@users.noreply.github.com>
2023-06-23 05:25:06 -0300

    calcular_valor_esperado.py

--- calcular_valor_esperado.py
+++ calcular_valor_esperado.py
@@ -5,14 +5,14 @@ def calcular_valor_esperado(chance):
         return "A chance deve ser maior que zero."
     else:
         valor_esperado = 1 / (chance / 100)
-        return f"O valor esperado é: {valor_esperado}"
+        return f"O valor esperado é: {round(valor_esperado, 2)}"
 
 def main():
-    st.title("Cálculo do Valor Esperado")
+    st.title("Valor Esperado")
     chance = st.number_input("Informe a chance:", min_value=0.01, max_value=100.0, value=50.0, step=0.01)
     valor_esperado = calcular_valor_esperado(chance)
 
-    st.write(valor_esperado)
+    st.write(Odd_esperada)
 
 if __name__ == "__main__":
     main()
