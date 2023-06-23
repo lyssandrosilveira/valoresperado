54dd9c49ea084644fba04c97ebd80fcf5ca5a118
lyssandrosilveira <45230721+lyssandrosilveira@users.noreply.github.com>
2023-06-23 05:27:49 -0300

    calcular_valor_esperado.py

--- calcular_valor_esperado.py
+++ calcular_valor_esperado.py
@@ -1,18 +1,18 @@
 import streamlit as st
 
-def calcular_valor_esperado(chance):
+def calcular_odd_esperada(chance):
     if chance <= 0:
         return "A chance deve ser maior que zero."
     else:
-        valor_esperado = 1 / (chance / 100)
-        return f"O valor esperado é: {round(valor_esperado, 2)}"
+        odd_esperada = 1 / (chance / 100)
+        return f"Odd esperada: {round(odd_esperada, 2)}"
 
 def main():
-    st.title("Cálculo do Valor Esperado")
+    st.title("Cálculo da Odd Esperada")
     chance = st.number_input("Informe a chance:", min_value=0.01, max_value=100.0, value=50.0, step=0.01)
-    valor_esperado = calcular_valor_esperado(chance)
+    odd_esperada = calcular_odd_esperada(chance)
 
-    st.write(ODD_esperada)
+    st.write(odd_esperada)
 
 if __name__ == "__main__":
     main()
